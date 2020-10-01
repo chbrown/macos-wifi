cc1302b92bb8ac32633c96a1b9c967b0ec95c24e
Christopher Brown <io@henrian.com>
2020-10-01 10:31:56 -0400

    Reformat lib and main files
    Again, just run swiftformat with all defaults

--- lib.swift
+++ lib.swift
@@ -1,55 +1,55 @@
 import Foundation
 
 /**
-Write the given string to the specified FileHandle.
+ Write the given string to the specified FileHandle.
 
-- parameter str: native string to write encoded in utf-8.
-- parameter handle: FileHandle to write to (defaults to /dev/stdout)
-- parameter terminator: string to write after `str` (encoded in utf-8) (defaults to a single newline)
-*/
-private func writeString(_ str: String,
-                         handle: FileHandle = .standardOutput,
-                         terminator: String = "\n") {
-  if let data = str.data(using: .utf8) {
-    handle.write(data)
-    if let terminatorData = terminator.data(using: .utf8) {
-      handle.write(terminatorData)
+ - parameter str: native string to write encoded in utf-8.
+ - parameter handle: FileHandle to write to (defaults to /dev/stdout)
+ - parameter terminator: string to write after `str` (encoded in utf-8) (defaults to a single newline)
+ */
+private func writeString(_ str: String, handle: FileHandle = .standardOutput, terminator: String = "\n") {
+    if let data = str.data(using: .utf8) {
+        handle.write(data)
+        if let terminatorData = terminator.data(using: .utf8) {
+            handle.write(terminatorData)
+        }
     }
-  }
 }
+
 func printOut(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-  let str = items.map { ($0 as AnyObject).description }.joined(separator: separator)
-  writeString(str, handle: .standardOutput, terminator: terminator)
+    let str = items.map { ($0 as AnyObject).description }.joined(separator: separator)
+    writeString(str, handle: .standardOutput, terminator: terminator)
 }
+
 func printErr(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-  let str = items.map { ($0 as AnyObject).description }.joined(separator: separator)
-  writeString(str, handle: .standardError, terminator: terminator)
+    let str = items.map { ($0 as AnyObject).description }.joined(separator: separator)
+    writeString(str, handle: .standardError, terminator: terminator)
 }
 
 private func inferColumns(_ dictionaries: [[String: String]]) -> [String] {
-  let keySet = dictionaries.reduce(Set<String>()) { keys, dictionary in
-    keys.union(dictionary.keys)
-  }
-  return Array(keySet)
+    let keySet = dictionaries.reduce(Set<String>()) { keys, dictionary in
+        keys.union(dictionary.keys)
+    }
+    return Array(keySet)
 }
-func formatTable(_ dictionaries: [[String: String]],
-                 keys: [String]? = nil) -> String {
-  let columns = keys ?? inferColumns(dictionaries)
-  // generate header row
-  let header = columns.joined(separator: "\t")
-  // reduce other dictionaries into single array
-  let rows = dictionaries.reduce([header]) { rows, dictionary in
-    let cells: [String] = columns.map { dictionary[$0] ?? "" }
-    let row = cells.joined(separator: "\t")
-    return rows + [row]
-  }
-  return rows.joined(separator: "\n")
+
+func formatTable(_ dictionaries: [[String: String]], keys: [String]? = nil) -> String {
+    let columns = keys ?? inferColumns(dictionaries)
+    // generate header row
+    let header = columns.joined(separator: "\t")
+    // reduce other dictionaries into single array
+    let rows = dictionaries.reduce([header]) { rows, dictionary in
+        let cells: [String] = columns.map { dictionary[$0] ?? "" }
+        let row = cells.joined(separator: "\t")
+        return rows + [row]
+    }
+    return rows.joined(separator: "\n")
 }
 
 func formatKVTable(_ dictionary: [String: String]) -> String {
-  let keyWidth = dictionary.keys.map { $0.count }.max() ?? 0
-  let lines = dictionary.map { entry in
-    "\(entry.key.padding(toLength: keyWidth, withPad: " ", startingAt: 0)): \(entry.value)"
-  }
-  return lines.joined(separator: "\n")
+    let keyWidth = dictionary.keys.map { $0.count }.max() ?? 0
+    let lines = dictionary.map { entry in
+        "\(entry.key.padding(toLength: keyWidth, withPad: " ", startingAt: 0)): \(entry.value)"
+    }
+    return lines.joined(separator: "\n")
 }
--- main.swift
+++ main.swift
@@ -1,18 +1,18 @@
-import Foundation
 import CoreWLAN
+import Foundation
 
 // CLI helpers
 
 private func basename(_ pathOption: String?) -> String? {
-  if let path = pathOption {
-    return URL(fileURLWithPath: path).lastPathComponent
-  }
-  return nil
+    if let path = pathOption {
+        return URL(fileURLWithPath: path).lastPathComponent
+    }
+    return nil
 }
 
 private func printUsage() {
-  let process = basename(CommandLine.arguments.first) ?? "executable"
-  printErr("""
+    let process = basename(CommandLine.arguments.first) ?? "executable"
+    printErr("""
     Usage: \(process) -h|-help|--help
            \(process) -action interfaces|current|scan
            \(process) # same as: -action current
@@ -23,83 +23,83 @@ private func printUsage() {
 // CLI actions
 
 func interfaces() {
-  printErr("Available interfaces:")
-  for interfaceName in CWWiFiClient.interfaceNames() ?? [] {
-    printOut(interfaceName)
-  }
+    printErr("Available interfaces:")
+    for interfaceName in CWWiFiClient.interfaceNames() ?? [] {
+        printOut(interfaceName)
+    }
 }
 
 func current(_ interface: CWInterface) {
-  printErr("Current interface:")
-  printOut(formatKVTable(interfaceDictionary(interface)))
+    printErr("Current interface:")
+    printOut(formatKVTable(interfaceDictionary(interface)))
 }
 
 func scan(_ interface: CWInterface) throws {
-  printErr("Available networks:")
-  let networks = try interface.scanForNetworks(withSSID: nil)
-  let networkDictionaries = networks.map(networkDictionary)
-  let keys = [
-    "SSID", "BSSID",
-    "ChannelNumber", "ChannelBand", "ChannelWidth",
-    "RSSI",
-    "Noise",
-    // "InformationElement",
-    // "BeaconInterval",
-    // "IBSS",
-    "Country",
-  ]
-  printOut(formatTable(Array(networkDictionaries), keys: keys))
+    printErr("Available networks:")
+    let networks = try interface.scanForNetworks(withSSID: nil)
+    let networkDictionaries = networks.map(networkDictionary)
+    let keys = [
+        "SSID", "BSSID",
+        "ChannelNumber", "ChannelBand", "ChannelWidth",
+        "RSSI",
+        "Noise",
+        // "InformationElement",
+        // "BeaconInterval",
+        // "IBSS",
+        "Country",
+    ]
+    printOut(formatTable(Array(networkDictionaries), keys: keys))
 }
 
 func associate(_ interface: CWInterface, bssid: String, password: String?) throws {
-  let networks = try interface.scanForNetworks(withSSID: nil)
-  printErr("Associating with bssid:", bssid)
-  if let targetNetwork = networks.first(where: {$0.bssid == bssid}) {
-    try interface.associate(to: targetNetwork, password: password)
-  } else {
-    printErr("No network matching bssid found!")
-  }
+    let networks = try interface.scanForNetworks(withSSID: nil)
+    printErr("Associating with bssid:", bssid)
+    if let targetNetwork = networks.first(where: { $0.bssid == bssid }) {
+        try interface.associate(to: targetNetwork, password: password)
+    } else {
+        printErr("No network matching bssid found!")
+    }
 }
 
 // CLI entry point
 
 func main() {
-  // CommandLine.arguments[0] is the path of the executed file, which we drop
-  let args = Array(CommandLine.arguments.dropFirst())
-  // handle boolean arguments separately since UserDefaults only processes '-key value' pairs
-  if (args.contains { arg in arg == "-h" || arg == "-help" || arg == "--help" }) {
-    printUsage()
-    exit(0)
-  }
-  // use UserDefaults to parse other command line arguments
-  let defaults = UserDefaults.standard
-
-  let action = defaults.string(forKey: "action") ?? "current"
-
-  let client = CWWiFiClient.shared()
-  let interface = client.interface()!
-
-  switch action {
-  case "interfaces":
-    interfaces()
-  case "current":
-    current(interface)
-  case "scan":
-    try! scan(interface)
-  case "associate":
-    if let bssid = defaults.string(forKey: "bssid") {
-      let password = defaults.string(forKey: "password")
-      try! associate(interface, bssid: bssid, password: password)
-    } else {
-      printErr("The 'associate' action requires supplying a -bssid value")
-      printUsage()
-      exit(1)
+    // CommandLine.arguments[0] is the path of the executed file, which we drop
+    let args = Array(CommandLine.arguments.dropFirst())
+    // handle boolean arguments separately since UserDefaults only processes '-key value' pairs
+    if (args.contains { arg in arg == "-h" || arg == "-help" || arg == "--help" }) {
+        printUsage()
+        exit(0)
     }
-  default:
-    printErr("Unrecognized action: \(action)")
-    printUsage()
-    exit(1)
-  }
+    // use UserDefaults to parse other command line arguments
+    let defaults = UserDefaults.standard
+
+    let action = defaults.string(forKey: "action") ?? "current"
+
+    let client = CWWiFiClient.shared()
+    let interface = client.interface()!
+
+    switch action {
+    case "interfaces":
+        interfaces()
+    case "current":
+        current(interface)
+    case "scan":
+        try! scan(interface)
+    case "associate":
+        if let bssid = defaults.string(forKey: "bssid") {
+            let password = defaults.string(forKey: "password")
+            try! associate(interface, bssid: bssid, password: password)
+        } else {
+            printErr("The 'associate' action requires supplying a -bssid value")
+            printUsage()
+            exit(1)
+        }
+    default:
+        printErr("Unrecognized action: \(action)")
+        printUsage()
+        exit(1)
+    }
 }
 
 main()
