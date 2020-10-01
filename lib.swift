51fbfc5df76afd231d8f2c3658298cae4cfe4009
Christopher Brown <io@henrian.com>
2020-10-01 11:16:18 -0400

    Move format{,KV}Table from lib to new 'serialization' file

--- lib.swift
+++ lib.swift
@@ -25,31 +25,3 @@ func printErr(_ items: Any..., separator: String = " ", terminator: String = "\n
     let str = items.map { ($0 as AnyObject).description }.joined(separator: separator)
     writeString(str, handle: .standardError, terminator: terminator)
 }
-
-private func inferColumns(_ dictionaries: [[String: String]]) -> [String] {
-    let keySet = dictionaries.reduce(Set<String>()) { keys, dictionary in
-        keys.union(dictionary.keys)
-    }
-    return Array(keySet)
-}
-
-func formatTable(_ dictionaries: [[String: String]], keys: [String]? = nil) -> String {
-    let columns = keys ?? inferColumns(dictionaries)
-    // generate header row
-    let header = columns.joined(separator: "\t")
-    // reduce other dictionaries into single array
-    let rows = dictionaries.reduce([header]) { rows, dictionary in
-        let cells: [String] = columns.map { dictionary[$0] ?? "" }
-        let row = cells.joined(separator: "\t")
-        return rows + [row]
-    }
-    return rows.joined(separator: "\n")
-}
-
-func formatKVTable(_ dictionary: [String: String]) -> String {
-    let keyWidth = dictionary.keys.map { $0.count }.max() ?? 0
-    let lines = dictionary.map { entry in
-        "\(entry.key.padding(toLength: keyWidth, withPad: " ", startingAt: 0)): \(entry.value)"
-    }
-    return lines.joined(separator: "\n")
-}
