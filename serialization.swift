6c3e8b597199c3b958b106d963e26274b837fa72
Christopher Brown <io@henrian.com>
2020-10-02 09:04:32 -0400

    Support Dictionaries with 'Any?' values in formatters

--- serialization.swift
+++ serialization.swift
@@ -8,30 +8,37 @@ enum Format: String {
 
 private let newline = Data([0x0A])
 
-private func inferColumns(_ dictionaries: [[String: String]]) -> [String] {
+func formatValue(_ value: Any?, nilValue: String = "N/A") -> String {
+    if let nonNilValue = value {
+        return String(describing: nonNilValue)
+    }
+    return nilValue
+}
+
+private func inferColumns(_ dictionaries: [[String: Any?]]) -> [String] {
     let keySet = dictionaries.reduce(Set<String>()) { keys, dictionary in
         keys.union(dictionary.keys)
     }
     return Array(keySet)
 }
 
-func formatTable(_ dictionaries: [[String: String]], keys: [String]? = nil) -> String {
+func formatTable(_ dictionaries: [[String: Any?]], keys: [String]? = nil) -> String {
     let columns = keys ?? inferColumns(dictionaries)
     // generate header row
     let header = columns.joined(separator: "\t")
     // reduce other dictionaries into single array
     let rows = dictionaries.reduce([header]) { rows, dictionary in
-        let cells: [String] = columns.map { dictionary[$0] ?? "" }
+        let cells: [String] = columns.map { formatValue(dictionary[$0] ?? nil) }
         let row = cells.joined(separator: "\t")
         return rows + [row]
     }
     return rows.joined(separator: "\n")
 }
 
-func formatKVTable(_ dictionary: [String: String]) -> String {
+func formatKVTable(_ dictionary: [String: Any?]) -> String {
     let keyWidth = dictionary.keys.map { $0.count }.max() ?? 0
     let lines = dictionary.map { entry in
-        "\(entry.key.padding(toLength: keyWidth, withPad: " ", startingAt: 0)): \(entry.value)"
+        "\(entry.key.padding(toLength: keyWidth, withPad: " ", startingAt: 0)): \(formatValue(entry.value))"
     }
     return lines.joined(separator: "\n")
 }
@@ -56,9 +63,9 @@ func serialize(_ value: Any, format: Format = .tty, handle: FileHandle = .standa
             handle.write(newline)
         }
     case .tty:
-        if let dictionary = value as? [String: String] {
+        if let dictionary = value as? [String: Any?] {
             printLine(formatKVTable(dictionary), handle: handle)
-        } else if let dictionaries = value as? [[String: String]] {
+        } else if let dictionaries = value as? [[String: Any?]] {
             printLine(formatTable(dictionaries), handle: handle)
         } else if let array = value as? [String] {
             for item in array {
